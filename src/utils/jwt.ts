8c30768201a145e90da1db9e20e09421e6bb46b9
gian <gian@pop-os.localdomain>
2024-09-12 14:53:05 -0300

    remove console.log

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,7 +3,6 @@ import { Payload } from "./jwt.types";
 
 export async function verifyToken(token: string) {
   try {
-    console.log("JWTENV => ", process.env.JWT_SECRET);
     return jwt.verify(token, process.env.JWT_SECRET as string);
   } catch (error) {
     console.log(error);
